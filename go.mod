83eeff12d9c9c7f30f232ea1415c8c64d42e9465
bovarysme <bovarysme@users.noreply.github.com>
2019-09-21 23:41:40 +0200

    Implement repositoryHandler

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bovarysme/fudge
 go 1.13
 
 require (
+	github.com/dustin/go-humanize v1.0.0
 	github.com/gorilla/mux v1.7.3
 	gopkg.in/src-d/go-git.v4 v4.13.1
 )
--- main.go
+++ main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 
+	"github.com/dustin/go-humanize"
 	"github.com/gorilla/mux"
 	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
 )
+
+type fileInfo struct {
+	Name   string
+	IsFile bool
+	Size   string
+}
 
 func isRepository(filename string) (bool, error) {
 	path := filepath.Join(root, filename)
@@ -91,6 +100,74 @@ func repositoryHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+
+	path := filepath.Join(root, vars["repository"])
+	repository, err := git.PlainOpen(path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	head, err := repository.Head()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	commit, err := repository.CommitObject(head.Hash())
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	tree, err := commit.Tree()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	walker := object.NewTreeWalker(tree, false, nil)
+
+	var files []*fileInfo
+	for {
+		name, entry, err := walker.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		size, err := tree.Size(name)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		info := &fileInfo{
+			Name:   name,
+			IsFile: entry.Mode.IsFile(),
+			Size:   humanize.Bytes(uint64(size)),
+		}
+
+		files = append(files, info)
+	}
+
+	t, err := template.ParseFiles("template/repository.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := struct {
+		Name  string
+		Files []*fileInfo
+	}{
+		vars["repository"],
+		files,
+	}
+	t.Execute(w, params)
 }
 
 func main() {
@@ -103,5 +180,6 @@ func main() {
 	router.HandleFunc("/{repository}/blob/{commit}/{path:.*}", nil)
 	router.HandleFunc("/{repository}/raw/{commit}/{path:.*}", nil)
 
+	fmt.Println("Starting server on localhost:8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }

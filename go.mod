49bbe264ce3451650be3562a1a81a348f2838e99
bovarysme <bovarysme@users.noreply.github.com>
2019-09-21 22:43:20 +0200

    Implement homeHandler

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bovarysme/fudge
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/gorilla/mux v1.7.3
+	gopkg.in/src-d/go-git.v4 v4.13.1
+)
--- main.go
+++ main.go
@@ -2,25 +2,106 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
+	"gopkg.in/src-d/go-git.v4"
 )
 
+func isRepository(filename string) (bool, error) {
+	path := filepath.Join(root, filename)
+
+	file, err := os.Stat(path)
+	if err != nil {
+		return false, nil
+	}
+
+	if !file.IsDir() {
+		return false, nil
+	}
+
+	_, err = git.PlainOpen(path)
+	if err == git.ErrRepositoryNotExists {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func getRepositoryNames() ([]string, error) {
+	var names []string
+
+	files, err := ioutil.ReadDir(root)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, file := range files {
+		ok, err := isRepository(file.Name())
+		if err != nil {
+			return nil, err
+		}
+		if ok {
+			names = append(names, file.Name())
+		}
+	}
+
+	return names, nil
+}
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!")
+	names, err := getRepositoryNames()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	t, err := template.ParseFiles("template/home.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := struct {
+		Names []string
+	}{
+		names,
+	}
+	t.Execute(w, params)
+}
+
+func repositoryHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	ok, err := isRepository(vars["repository"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if !ok {
+		http.NotFound(w, r)
+		return
+	}
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.StrictSlash(true)
 	router.HandleFunc("/", homeHandler)
-	router.HandleFunc("/{repository}", homeHandler)
-	router.HandleFunc("/{repository}/commits/{branch}", homeHandler)
-	router.HandleFunc("/{repository}/tree/{commit}/{path:.*}", homeHandler)
-	router.HandleFunc("/{repository}/blob/{commit}/{path:.*}", homeHandler)
-	router.HandleFunc("/{repository}/raw/{commit}/{path:.*}", homeHandler)
+	router.HandleFunc("/{repository}", repositoryHandler)
+	router.HandleFunc("/{repository}/commits/{branch}", nil)
+	router.HandleFunc("/{repository}/tree/{commit}/{path:.*}", nil)
+	router.HandleFunc("/{repository}/blob/{commit}/{path:.*}", nil)
+	router.HandleFunc("/{repository}/raw/{commit}/{path:.*}", nil)
 
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }

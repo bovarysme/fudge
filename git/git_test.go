02366edb5c9fa9bc4c1f565db75b104780eb6b0c
bovarysme <bovarysme@users.noreply.github.com>
2019-10-24 16:43:04 +0200

    Test git.OpenRepository

---
+++ git/git_test.go
@@ -0,0 +1,30 @@
+package git
+
+import (
+	"testing"
+
+	"gopkg.in/src-d/go-git.v4"
+)
+
+func TestOpenRepository(t *testing.T) {
+	tests := []struct {
+		root    string
+		dirname string
+		strict  bool
+		err     error
+	}{
+		{"testdata/regular_files", "test", true, git.ErrRepositoryNotExists},
+		{"testdata/regular_files", "test", false, git.ErrRepositoryNotExists},
+		{"testdata/regular_file_with_repo", "test", true, git.ErrRepositoryNotExists},
+		{"testdata/regular_file_with_repo", "test.git", true, nil},
+		{"testdata/regular_file_with_repo", "test", false, nil},
+	}
+
+	for _, test := range tests {
+		_, err := OpenRepository(test.root, test.dirname, test.strict)
+		if err != test.err {
+			t.Errorf("error when openning %s/%s (strict: %v): got %v, want %v",
+				test.root, test.dirname, test.strict, err, test.err)
+		}
+	}
+}
